372955d94221de922d9dafd7faa0c52331dd6ae7
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-01 17:59:12 +0300

    Add App Component To The Project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import SearchBox from "./SearchBox";
+import Result from "./Result";
+import blacktrain from "./assets/blacktrain.jpg";
+import axios from "axios";
+
+function App() {
+  const [stations, setStations] = useState([]);
+  const [path, setPath] = useState([]);
+  const [source, setSource] = useState("");
+  const [destination, setDestination] = useState("");
+
+  useEffect(() => {
+    async function fetchStations() {
+      const stationList = await getStations();
+      setStations(stationList);
+    }
+    fetchStations();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const source = event.target.source.value;
+    const destination = event.target.destination.value;
+    let obj = { source, destination };
+    let objToServer = JSON.stringify(obj);
+    setSource(source);
+    setDestination(destination);
+    const pathData = await getPath(objToServer);
+    setPath(pathData);
+  };
+
+  return (
+    <>
+      <div className="nav">
+        <h1>Metro Station Finder</h1>
+        <div className="icon-container">
+          <img src={blacktrain} alt="train" className="icon" />
+        </div>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <div className="container">
+          <SearchBox place="Source :" name="source" arr={stations} />
+          <SearchBox place="Destination :" name="destination" arr={stations} />
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+
+      <Result source={source} destination={destination} stations={path} />
+    </>
+  );
+}
+
+export default App;
